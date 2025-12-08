79b93174eb09a78a23150c106308db87100e67a0
Hyesu-Choi <hsc1263@gamil.com>
2025-12-08 13:58:42 +0900

    test

---
+++ src/C01Basic/C01InputOutput.java
@@ -0,0 +1,11 @@
+package C01Basic;
+
+public class C01InputOutput {
+    public static void main(String[] args) {
+//        출력 : System.out을 통해 콘솔(터미널창) 출력
+        System.out.println(20); // 1)println은 줄바꿈 있는 출력. 20을 출력하고 줄바꿈함. 2)입출력은 모두 문자열로 진행
+        System.out.print("hello world1");  // print는 줄바꿈이 없는 출력
+        System.out.print("hello world2");
+
+    }
+}
